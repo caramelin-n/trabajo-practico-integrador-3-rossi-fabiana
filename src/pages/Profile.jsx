475dd85c40c05db52d783ff36582faee3dfb2287
caramelin-n <rossifabiana930@gmail.com>
2025-11-27 19:30:31 -0300

    pagina de profile implementada

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useState } from "react"
+import { useEffect } from "react";
+import { Loading } from "../components/Loading"
 
 const Profile = () => {
+  const [profile, setProfile] = useState({
+    id: "",
+    name: "",
+    lastname: ""
+  });
+  const [isLoading, setIsLoading] = useState(false);
+    useEffect(()=>{
+        const fetchData = async () =>{
+          try {
+            setIsLoading(true)
+            const res = await fetch("http://localhost:3000/api/profile",
+              {method: "GET", credentials: "include"}
+            )
+            const data = await res.json()
+            console.log(data.user);
+            setProfile({
+              id: data.user.id,
+              name: data.user.name,
+              lastname: data.user.lastname
+            })
+          } catch (error) {
+            console.log(error);
+            setIsLoading(false)
+          } finally{
+            setIsLoading(false)
+          }
+        };
+        fetchData();
+      },[]);
+
+  if(isLoading){
+    return(<Loading/>)
+  }
   return (
     <div>
-      
+      <h1>Perfil</h1>
+      <p>Id: {profile.id}</p>
+      <p>Nombre: {profile.name}</p>
+      <p>Apellido: {profile.lastname}</p>
     </div>
   )
 }
